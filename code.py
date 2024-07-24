0ee190908077ac64f6afd1f1575e24a825fc7630
Leland Sindt <leland@sway.org>
2024-07-23 20:33:07 -0500

    provide status via colors

--- code.py
+++ code.py
@@ -9,6 +9,7 @@ import board
 import time 
 import supervisor
 import traceback
+import neopixel
 
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
@@ -16,6 +17,13 @@ ORANGE = (255,165,0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
+PURPLE = (127, 0, 255)
+PINK = (255, 0, 255)
+
+num_pixels = 64
+ORDER = neopixel.RGB
+pixel = neopixel.NeoPixel(board.D14, num_pixels, brightness=0.05, pixel_order=ORDER)
+pixel.fill(PINK)
 
 print()
 print("Connecting to WiFi")
@@ -31,11 +39,6 @@ print("My MAC addr:", [hex(i) for i in wifi.radio.mac_address])
 #  prints IP address to REPL
 print("My IP address is", wifi.radio.ipv4_address)
 
-import neopixel
-
-num_pixels = 64
-ORDER = neopixel.RGB
-pixel = neopixel.NeoPixel(board.D14, num_pixels, brightness=0.05, pixel_order=ORDER)
 pixel.fill(BLUE)
 #pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
 
@@ -76,16 +79,16 @@ while True:
             
     if severity == 0:
         COLOR = GREEN
-    if severity == 1: 
+    elif severity == 1:
         COLOR = YELLOW
-    if severity == 2: # Warning (yellow in logic monitor)
+    elif severity == 2: # Warning (yellow in logic monitor)
         COLOR = ORANGE
-    if severity == 3: # Error
+    elif severity == 3: # Error
         COLOR = RED
-    if severity == 4: # Critical (red/fire in logic monitor)
+    elif severity == 4: # Critical (red/fire in logic monitor)
         COLOR = RED
-    # add and else/default of purple in case logic monitor starts snding unknown severites?
-
+    else:
+        COLOR = PURPLE
 
     pixel.fill(COLOR)
 
